d4b532bc90901e5e70a1ac5399965429fada3d8f
Jakub Boros <enginedevel@gmail.com>
2021-07-23 15:23:25 +0200

    Made bag.h more portable.

--- bag.h
+++ bag.h
@@ -1,14 +1,16 @@
 #ifndef BAG_INCLUDE_H
 #define BAG_INCLUDE_H
 
-typedef unsigned long u64;
-typedef signed long s64;
-typedef unsigned int u32;
-typedef signed int s32;
-typedef unsigned short u16;
-typedef signed short s16;
-typedef unsigned char u8;
-typedef signed char s8;
+#include <stdint.h>
+
+typedef uint64_t u64;
+typedef int64_t s64;
+typedef uint32_t u32;
+typedef int32_t s32;
+typedef uint16_t u16;
+typedef int16_t s16;
+typedef uint8_t u8;
+typedef int8_t s8;
 
 typedef float f32;
 typedef double f64;
